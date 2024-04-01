972c4be9aab29ed1a40cbbdb370c83fa0314f776
l3xst1ng <howdy.martt@gmail.com>
2024-04-01 20:07:43 +0300

    Event listeners fix

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// function addingEventListener() {
+//   const input = document.getElementById("input");
+//   const alertMessage = () => alert("It actually works, awesome");
+//   input.addEventListener("click", alertMessage);
+// }
+
 function addingEventListener() {
+  const button = document.getElementById("button");
+  button.addEventListener("click", function () {
+    // Event listener logic
+  });
 }
